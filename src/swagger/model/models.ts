4186c4b14954f14e341163624f5a0f817ef6be8f
DJ Soults <dj@illuminateddev.com>
2018-03-23 10:02:46 -0500

    Add initial swagger codegen files, and npm task for generating

---
+++ src/swagger/model/models.ts
@@ -0,0 +1,7 @@
+export * from './checkIn';
+export * from './loginToken';
+export * from './organization';
+export * from './project';
+export * from './user';
+export * from './userOrganization';
+export * from './userProject';
