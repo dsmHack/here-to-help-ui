3950642330df0275a766a35fd333d763fa668b5c
Gujja, Reddy <NGujja@wrberkley.com>
2018-03-23 10:25:34 -0500

    first dab at check-in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {CheckInComponent} from './check-in/checkin.component';
 
+const appRoutes: Routes = [
+  { path: 'check-in', component: CheckInComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, CheckInComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    BrowserModule, NgbModule.forRoot(), FormsModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
