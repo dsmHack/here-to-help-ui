a40ae5f75db6ba13f73feaa0a0aa146af0c49f95
Gujja, Reddy <NGujja@wrberkley.com>
2018-03-24 00:50:36 -0500

    integrated services

---
+++ src/app/organization-profile/organization-profile.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Organization} from '../../swagger';
+import {DefaultService} from '../../swagger';
+
+@Component({
+  selector: 'app-organization-profile',
+  templateUrl: './organization-profile.component.html',
+  styleUrls: ['./organization-profile.component.css'],
+  providers: [DefaultService]
+})
+export class OrganizationProfileComponent implements OnInit {
+  orgs:Array<Organization>;
+  org = <Organization>{};
+
+  constructor(private route: ActivatedRoute, private service: DefaultService) {
+    this.service.organizationsGet().subscribe(orgs => console.log(orgs));
+
+    this.org = <Organization>{
+        name: 'Amanda the Panda!',
+        organizationId: '123',
+        description: 'Our Mission is to provide innovative grief support services that promote hope and healing to individuals, children, and families',
+        email: 'you@gmail.com',
+        phoneNumber: '208 393 4192',
+        websiteUrl: 'www.google.com',
+        facebookUrl: 'fb.com',
+        twitterUrl: 'twitter.com',
+        instagramUrl: 'instagram.com'
+      };
+  }
+
+  ngOnInit() {
+  }
+
+}
