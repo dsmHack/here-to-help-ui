710058e2771ec6ea7946ea100eb0fb31f977a35b
DJ Soults <dj@illuminateddev.com>
2018-03-24 08:44:34 -0500

    Update for successful api testing

--- src/app/organization-profile/organization-profile.component.ts
+++ src/app/organization-profile/organization-profile.component.ts
@@ -10,24 +10,23 @@ import {DefaultService} from '../../swagger';
   providers: [DefaultService]
 })
 export class OrganizationProfileComponent implements OnInit {
-  orgs: Array<Organization>;
   org = <Organization>{};
 
   constructor(private route: ActivatedRoute, private service: DefaultService) {
-    this.service.organizationsGet().subscribe(orgs => console.log(orgs));
+    this.service.organizationsGet().subscribe(x => this.org = x[0]);
 
-    this.org = <Organization>{
-        name: 'Amanda the Panda!',
-        organizationId: '123',
-        description: 'Our Mission is to provide innovative grief support services ' +
-        'that promote hope and healing to individuals, children, and families',
-        email: 'you@gmail.com',
-        phoneNumber: '208 393 4192',
-        websiteUrl: 'www.google.com',
-        facebookUrl: 'fb.com',
-        twitterUrl: 'twitter.com',
-        instagramUrl: 'instagram.com'
-      };
+    // this.org = <Organization>{
+    //     name: 'Amanda the Panda!',
+    //     organizationId: '123',
+    //     description: 'Our Mission is to provide innovative grief support services ' +
+    //     'that promote hope and healing to individuals, children, and families',
+    //     email: 'you@gmail.com',
+    //     phoneNumber: '208 393 4192',
+    //     websiteUrl: 'www.google.com',
+    //     facebookUrl: 'fb.com',
+    //     twitterUrl: 'twitter.com',
+    //     instagramUrl: 'instagram.com'
+    //   };
   }
 
   ngOnInit() {
